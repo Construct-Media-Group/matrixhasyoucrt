7cbf9346ee0033b890986cf8e278fe808e8f8cd1
Elliot-Construct <72305414+Elliot-Construct@users.noreply.github.com>
2023-09-27 20:41:30 +0100

    updated chat interface

--- commands/chat/index.mjs
+++ commands/chat/index.mjs
@@ -1,76 +1,33 @@
 // chat.mjs
 
-import { getScreen, clear } from "../../util/screens.js";
+import { prompt, type } from "../../util/io.js"; 
 
-async function chat() {
-  clear();
+export default async function chat() {
 
-  return new Promise((resolve) => {
-    let container = getScreen();
-    container.setAttribute("contenteditable", false);
-    container.focus();
+  const socket = new WebSocket("ws://cdeg.constructdiversitygroups.com:5543");
 
-    const messageInput = document.createElement("input");
-    messageInput.id = "message-input";
-    messageInput.placeholder = "Type your message here...";
-    container.appendChild(messageInput);
-
-    // Create WebSocket connection
-    const socket = new WebSocket("ws://cdeg.constructdiversitygroups.com:5543");
-
-    // Connection opened
-    socket.addEventListener("open", (event) => {
-      console.log("WebSocket connection opened:", event);
-    });
-
-    // Listen for messages from the server
-    socket.addEventListener("message", (event) => {
-      const messageData = JSON.parse(event.data);
-      receiveMessage(container, { role: "assistant", text: messageData.text });
-    });
-
-    // Connection closed
-    socket.addEventListener("close", (event) => {
-      console.log("WebSocket connection closed:", event);
-    });
-
-    // Connection error
-    socket.addEventListener("error", (event) => {
-      console.log("WebSocket error:", event);
-    });
-
-    messageInput.addEventListener("keydown", (event) => {
-      if (event.key === "Enter") {
-        event.preventDefault();
-        const text = messageInput.value.trim();
-        if (text === "") return;
-
-        // Display user message
-        receiveMessage(container, { role: "user", text });
-
-        // Send message to the server
-        socket.send(JSON.stringify({ text }));
-
-        // Clear input
-        messageInput.value = "";
-      }
-    });
+  socket.addEventListener('open', () => {
+    type('Connected to chat server!');
   });
-}
 
-// Receive message and display in the container
-function receiveMessage(container, message) {
-  const messageElement = document.createElement("div");
-  messageElement.className = "chat-message";
+  socket.addEventListener('message', e => {
+    type(`Assistant: ${e.data}`);
+  });
 
-  if (message.role === "user") {
-    messageElement.innerHTML = `<strong>You:</strong> ${message.text}`;
-  } else {
-    messageElement.innerHTML = `<strong>The Oracle:</strong> ${message.text}`;
+  socket.addEventListener('close', () => {
+    type('Disconnected from server.');
+  });
+
+  while(true) {
+    
+    const message = await prompt('You: ');
+
+    if(message === 'exit') break;
+
+    socket.send(message);
+
   }
 
-  container.appendChild(messageElement);
-  container.scrollTop = container.scrollHeight;
-}
+  socket.close();
 
-export default chat;
+}
