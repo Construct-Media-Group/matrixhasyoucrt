b2c1f17f8e0b9d0fb2b14cc6def3777c95145ba2
Elliot-Construct <Elliot@constructsound.com>
2023-12-07 00:56:45 +0000

    Added a waiting section whilst waiting for the prompt response.

--- commands/chat/index.mjs
+++ commands/chat/index.mjs
@@ -7,24 +7,27 @@ const API_URL = 'https://oracle.thematrixhasyou.tech/chat';
 export default async function chat() {
   while(true) {
     const message = await prompt('You: ');
-
+    console.log(`Simulant: ${message}`)
     if(message === 'exit') break;
-      
+
+    type('WAITING...');
+
     const response = await fetch(API_URL, {
       method: 'POST',
       credentials: 'include',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({message})  // sessionId will be automatically included because credentials are set to 'include'
+      body: JSON.stringify({message: message})  // sessionId will be automatically included because credentials are set to 'include'
     });
     // Parse the JSON response body
-    const data = await response.json();
+
+    const responseData = await response.json();
 
     // Extract the 'message' field from the JSON response
-    const assistantResponse = data.message;
+    const assistantResponse = JSON.stringify(responseData.message);
 
-    console.log({assistantResponse});
-    type(`Assistant: ${assistantResponse}`);  
+    console.log(`Oracle: ${assistantResponse}`);
+    await type(`Assistant: ${assistantResponse}`);  
   }
 } 
